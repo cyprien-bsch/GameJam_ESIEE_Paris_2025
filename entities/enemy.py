267634211f37c61aa628caee428c59331e498671
LucieSouiou <luciesouiou@gmail.com>
2025-09-17 10:33:16 +0200

    déplacements des enemis avec targets

--- entities/enemy.py
+++ entities/enemy.py
@@ -2,6 +2,7 @@ import arcade
 import settings
 from enum import Enum
 import random
+import math
 from entities.projectiles import Projectile
 
 class Direction(Enum):
@@ -9,21 +10,52 @@ class Direction(Enum):
     BOTTOM = 2
     RIGHT = 3
     TOP = 4
+    TARGET = 5
 
 class Enemy(arcade.Sprite):
-    def __init__(self, x: float, y: float, direction: Direction = Direction.LEFT, projectiles: arcade.SpriteList = None):
+    def __init__(self, x: float, y: float, direction: Direction = Direction.LEFT, projectiles: arcade.SpriteList = None, targets: arcade.SpriteList = None):
         super().__init__(":resources:images/enemies/fishGreen.png", 1.0)
         self.center_x = x
         self.center_y = y
         self.direction = direction
         self.moving = 0
+        self.target = None
         self.projectiles = projectiles if projectiles is not None else arcade.SpriteList()
+        self.targets = targets if targets is not None else arcade.SpriteList()
+        self.target_distance_limit = 300
+
+
+    def distance(self, enemy):
+        x1, y1 = self.center_x, self.center_y
+        x2, y2 = enemy.center_x, enemy.center_y
+
+        return math.sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
+    
+
+    def nearest_target(self):
+        current_distance, minimal_distance = 0, self.target_distance_limit
+        nearest_target = None
+
+        for target in self.targets:
+            current_distance = self.distance(self.targets)
+            if (current_distance < minimal_distance):
+                nearest_target = target
+                current_distance = minimal_distance
+        
+        if nearest_target is not None:
+            return self.center_x - nearest_target.center_x, self.center_y - nearest_target.center_y
+        return None
 
 
     def update_direction(self):
         self.moving = 0
-        
-        new_dir = round(random.random())
+
+        self.target = self.nearest_target()
+
+        if (self.target is not None):
+            self.direction = Direction.TARGET
+        else:
+            new_dir = round(random.random())
 
         if self.direction == Direction.RIGHT:
             self.direction = Direction.BOTTOM if new_dir == 0 else Direction.TOP
@@ -46,6 +78,10 @@ class Enemy(arcade.Sprite):
             self.center_y += 1
         elif self.direction == Direction.TOP:
             self.center_y -= 1
+        
+        elif self.direction == Direction.TARGET:
+            self.center_x += self.target[0]
+            self.center_y += self.target[1]
             
         if self.center_x < 0:
             self.center_x = settings.SCREEN_WIDTH
